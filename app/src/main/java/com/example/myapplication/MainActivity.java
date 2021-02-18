b12deeb6b8568153570e548d6101a913f3cf5a7d
ashwin <ashwintlp98@gmail.com>
2021-02-18 12:21:45 +0530

    Fertilizer page initial

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -9,70 +9,68 @@ import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
-
     DrawerLayout drawerLayout;
     NavigationView navigationView;
     Toolbar toolbar;
+    Menu menu;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        drawerLayout= findViewById(R.id.drawer_layout);
+        drawerLayout=findViewById(R.id.drawer_layout);
         navigationView=findViewById(R.id.nav_view);
+        textView=findViewById(R.id.textView);
         toolbar=findViewById(R.id.toolbar);
 
-
-        setSupportActionBar(toolbar);
-
         navigationView.bringToFront();
-
-        ActionBarDrawerToggle toggle=new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        ActionBarDrawerToggle toggle=new
+                ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
-navigationView.setNavigationItemSelectedListener(this);
+        navigationView.setNavigationItemSelectedListener(this);
+        navigationView.setCheckedItem(R.id.nav_home);
     }
 
     @Override
-
-    public void onBackPressed() {
-        if(drawerLayout.isDrawerOpen(GravityCompat.START))
-        {
+    public void onBackPressed(){
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
             drawerLayout.closeDrawer(GravityCompat.START);
         }
-        else {
-            super.onBackPressed();
+        else
+        {super.onBackPressed();
         }
-
-
     }
 
     @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-
-        switch (item.getItemId()){
-            case R.id.nav_home:
-                     break;
-            case R.id.nav_contact:
-                    Intent intent=new Intent(MainActivity.this,ThirdActivity.class);
-                    startActivity(intent);
-                    break;
-            case  R.id.nav_help:
-                Toast.makeText(this,"help",Toast.LENGTH_SHORT).show();
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+            case R.id.nav_home: break; case R.id.announcement:
+                Intent intent = new Intent(MainActivity.this, ThirdActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.nav_view: menu.findItem(R.id.nav_view).setVisible(true);
+                menu.findItem(R.id.nav_help).setVisible(true);
+                menu.findItem(R.id.nav_contact).setVisible(false);
                 break;
 
 
-
-            }
-
-        return true;
+            case R.id.nav_contactphone: menu.findItem(R.id.nav_contactphone).setVisible(false);
+                menu.findItem(R.id.nav_help).setVisible(false);
+                menu.findItem(R.id.nav_view).setVisible(true);
+                break;
+            case R.id.nav_help: Toast.makeText(this, "We are Here to help you!", Toast.LENGTH_SHORT).show(); break;
+        }
+        drawerLayout.closeDrawer(GravityCompat.START); return true;
     }
-}
+    }
